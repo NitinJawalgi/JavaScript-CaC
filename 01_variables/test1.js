d3dc88b4728ccbd0561d3a9cf3c7667d1549b5af
NitinJawalgi <161961261+NitinJawalgi@users.noreply.github.com>
2024-09-14 08:37:07 +0000

    container config done

---
+++ 01_variables/test1.js
@@ -0,0 +1,20 @@
+const accountID = 1234534
+let phone = 984491099
+var accountName = "sachin"
+accountEmail = "abc@gmail.com"
+
+console.log(accountID);
+console.log(phone);
+console.log(accountName);
+console.log(accountEmail);
+
+accountName = "Nitin"
+phone = 123456789
+//const accountID = 345
+//Cannot redeclare block-scoped variable 'accountID
+
+console.log(accountID);
+console.log(phone);
+
+
+
