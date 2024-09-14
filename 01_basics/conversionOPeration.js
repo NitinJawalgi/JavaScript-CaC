3b757eb90eb9cd4ec64180f5172ab554a9aaf930
NitinJawalgi <161961261+NitinJawalgi@users.noreply.github.com>
2024-09-14 10:24:18 +0000

    Conversion Operations

---
+++ 01_basics/conversionOPeration.js
@@ -0,0 +1,80 @@
+let score = "33abc"
+let score1=null
+let score2 = undefined
+let score3 = true 
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));//number
+console.log(valueInNumber);//NaN
+console.log("-----------------------");
+
+//score1
+console.log(typeof score1);
+console.log(typeof(score1));
+
+let valueInNumber1 = Number(score1)
+console.log(typeof(valueInNumber1));//number
+console.log(valueInNumber1);//NaN
+console.log("-----------------------");
+//score3
+
+console.log(typeof score2);
+console.log(typeof(score2));
+
+let valueInNumber2 = Number(score2)
+console.log(typeof(valueInNumber2));//number
+console.log(valueInNumber2);//NaN
+console.log("-----------------------");
+
+//score 3
+console.log(typeof score3);
+console.log(typeof(score3));
+
+let valueInNumber3 = Number(score3)
+console.log(typeof(valueInNumber3));
+console.log(valueInNumber3);
+console.log("-----------------------");
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+//let isLoggedIn = 1
+let isLoggedIn=""
+let booleanisLoogedIn = Boolean(isLoggedIn)
+console.log(booleanisLoogedIn)
+console.log("-----------------------");
+//1==>true; 0=> false
+// " " => false
+//"hitesh" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof(stringNumber));
+ console.log("-----------------------");
+
+ /*********************operations******************* */
+
+ let value = 3
+ let negValue= -value
+ //console.log(negValue);
+
+ let str1="hello"
+ let str2=" harry"
+ let str3= str1 + str2
+ console.log(str3);
+
+ console.log("1"+2)
+ console.log(1+"2")
+ console.log("1"+2+2); 
+ console.log(1+2+"2");
+ 
+ 
+ 
+
+ 
