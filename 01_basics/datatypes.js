77752fb51781adda857b022b3a2c7edb398c2381
NitinJawalgi <161961261+NitinJawalgi@users.noreply.github.com>
2024-09-14 09:39:04 +0000

    File structue changes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,31 @@
+"use strict";
+
+//alert(3+3)
+//we are using nodejs, not browser
+
+console.log(3+3); 
+console.log("hello");
+
+let age = 18
+let isLoggedIn = false
+let state;
+
+//data types
+
+//1.number
+//big int
+//string => ""
+//boolean => true/false
+//null => stand alone value
+//symbol => unique
+
+//Objects
+
+console.log(typeof "hello");
+console.log(typeof null);//null is also treated as an object
+console.log(typeof undefined);
+
+
+
+
+
